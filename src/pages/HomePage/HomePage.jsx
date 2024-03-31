63675b51de4b1fb521ac0f10a4771aaa286cd601
Pavel <Pavel.Pasechnik@gmail.com>
2024-03-31 19:17:44 +0300

    bug fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import css from './HomePage.module.css';
+import postImg from '../../../public/phone.jpg';
 
 export default function HomePage() {
   return (
     <div className={css.container}>
       <h1>Welcome to the contact book!</h1>
-      <img src='../../../public/phone.jpg' alt='phone' width='300' height='300' />
+      <img src={postImg} alt='phone' width='300' height='300' />
     </div>
   );
 }
