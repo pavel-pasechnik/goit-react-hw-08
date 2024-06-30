7822cffa2af66b885ebf7f61e3bede7edf9cc8c5
Pavel <Pavel.Pasechnik@gmail.com>
2024-06-30 17:43:12 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,17 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      assets: '/src/assets',
-      modules: '/src/modules',
-      hooks: '/src/hooks',
-      pages: '/src/pages',
-      '@redux': '/src/redux',
-      components: '/src/components',
-      helpers: '/src/helpers',
-      context: '/src/context',
-      services: '/src/services',
-    },
-  },
+  plugins: [
+    react(),
+    eslintPlugin({
+      cache: false,
+      include: ['./src/**/*.js', './src/**/*.jsx'],
+      exclude: [],
+    }),
+  ],
   build: {
     sourcemap: true,
   },
